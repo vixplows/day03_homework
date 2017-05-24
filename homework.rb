d6c6146503616680123bb07805c483abe0cad45f
vixplows <vicky@vickyplows.co.uk>
2017-05-24 17:09:12 +0100

    complete part c homework.rb

--- homework.rb
+++ homework.rb
@@ -1,38 +1,38 @@
 # ### A. Given the following data structure:
 
-# lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
+lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
-# 1. Work out how many stops there are in the array
-# print lines.length()
+#1. Work out how many stops there are in the array
+print lines.length()
 
-# # 2. Return 'Edinburgh Park' from the array
-# print lines[1]
+# 2. Return 'Edinburgh Park' from the array
+print lines[1]
 
-# # 3. How many ways can we return 'Princes Street' from the array?
-# print lines[4]
-# print lines[-1]
-# print lines.last()
-# print lines.at(4)
+# 3. How many ways can we return 'Princes Street' from the array?
+print lines[4]
+print lines[-1]
+print lines.last()
+print lines.at(4)
 
-# # 4. Work out the index position of 'Haymarket'
-# print lines.index("Haymarket")
+# 4. Work out the index position of 'Haymarket'
+print lines.index("Haymarket")
 
-# # 5. Add 'Airport' to the start of the array
-# print lines.unshift("Airport")
+# 5. Add 'Airport' to the start of the array
+print lines.unshift("Airport")
 
-# # 6. Add 'York Place' to the end of the array
-# print lines.push("York Place")
+# 6. Add 'York Place' to the end of the array
+print lines.push("York Place")
 
-# # 7. Remove 'Edinburgh Park' from the array using it's name
-# print lines.delete("Edinburgh Park")
+# 7. Remove 'Edinburgh Park' from the array using it's name
+print lines.delete("Edinburgh Park")
 
-# # 8. Delete 'Edinburgh Park' from the array by index
-# print lines.delete_at(1)
+# 8. Delete 'Edinburgh Park' from the array by index
+print lines.delete_at(1)
 
-# # 9. Reverse the positions of the stops in the array
-# print lines.reverse
+# 9. Reverse the positions of the stops in the array
+print lines.reverse
 
-# ### B. Given the following data structure:
+### B. Given the following data structure:
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
@@ -88,19 +88,30 @@ users = {
 }
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print users["Jonathan"][:twitter]
 
 # 2. Return Erik's hometown
+print users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
+print users["Erik"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
+print users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
+print users["Erik"][:favourite_numbers][0]
 
 # 6. Add the number `7` to Erik's favorite numbers
+print users["Erik"][:favourite_numbers].push(7)
 
 # 7. Change Erik's hometown to Edinburgh
+#print users["Erik"][:home_town] = "Edinburgh"
+print users
 
 # 8. Add a pet dog to Erik called "Fluffy"
+print users["Erik"][:pets][:dog] = "Fluffy"
 
 # 9. Add yourself to the users hash
+print users["Victoria"] = {twitter: "vixplows", favourite_numbers: [10, 191], home_town: "Leeds", pets: {kiwi: "nosey"}}
+print users
