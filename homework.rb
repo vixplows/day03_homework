8da11e483b51f0ea9f8afcd045f491375ffa1480
vixplows <vicky@vickyplows.co.uk>
2017-05-24 16:34:58 +0100

    compelte part a & b in homework.rb

--- homework.rb
+++ homework.rb
@@ -1,50 +1,57 @@
-### A. Given the following data structure:
+# ### A. Given the following data structure:
 
-lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
+# lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
-1. Work out how many stops there are in the array
-print lines.length()
+# 1. Work out how many stops there are in the array
+# print lines.length()
 
-# 2. Return 'Edinburgh Park' from the array
-print lines[1]
+# # 2. Return 'Edinburgh Park' from the array
+# print lines[1]
 
-# 3. How many ways can we return 'Princes Street' from the array?
-print lines[4]
-print lines[-1]
-print lines.last()
-print lines.at(4)
+# # 3. How many ways can we return 'Princes Street' from the array?
+# print lines[4]
+# print lines[-1]
+# print lines.last()
+# print lines.at(4)
 
-# 4. Work out the index position of 'Haymarket'
-print lines.index("Haymarket")
+# # 4. Work out the index position of 'Haymarket'
+# print lines.index("Haymarket")
 
-# 5. Add 'Airport' to the start of the array
-print lines.unshift("Airport")
+# # 5. Add 'Airport' to the start of the array
+# print lines.unshift("Airport")
 
-# 6. Add 'York Place' to the end of the array
-print lines.push("York Place")
+# # 6. Add 'York Place' to the end of the array
+# print lines.push("York Place")
 
-# 7. Remove 'Edinburgh Park' from the array using it's name
-print lines.delete("Edinburgh Park")
+# # 7. Remove 'Edinburgh Park' from the array using it's name
+# print lines.delete("Edinburgh Park")
 
-# 8. Delete 'Edinburgh Park' from the array by index
-print lines.delete_at(1)
+# # 8. Delete 'Edinburgh Park' from the array by index
+# print lines.delete_at(1)
 
-# 9. Reverse the positions of the stops in the array
-print lines.reverse
+# # 9. Reverse the positions of the stops in the array
+# print lines.reverse
 
-### B. Given the following data structure:
+# ### B. Given the following data structure:
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
+print my_hash[1]
 
 # 2. How would you return the string `"Two"`?
+print my_hash[:two]
 
 # 3. How would you return the number `2`?
+print my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+print my_hash[3] = "Three"
 
 # 5. How would you add `{:four => 4}` to the hash?
+print my_hash[:four] = 4
+
+print my_hash
 
 ### C. Given the following data structure:
 
